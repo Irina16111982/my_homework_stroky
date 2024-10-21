99d67f58210316caa2163cc1a0eb9893566f4bfe
Irina16111982 <i.a.kaparushkina@tmn3.etagi.com>
2024-10-21 08:52:18 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-name='Кардиган'
-print(name[0]) # первый символ этой строки
-print(name[-1:]) #последний символ этой строки
-print(name[3:]) #вторую половину этой строки
-print(name[::-1]) #это слово наоборот
-print(name[0::2]) #каждый второй символ этой строки
+completed_homework=12
+zapyataya=','
+print(completed_homework)
+hours_spent=1.5
+print(hours_spent)
+name_cours='Python,'
+print(name_cours)
+time_for_one_task=0.1+0.025
+print(time_for_one_task)
+print('Курс:',name_cours,'всего задач:',completed_homework,zapyataya,'затрачено часов:',hours_spent,zapyataya,"среднее время выполнения:",time_for_one_task," часа.")
+#все
